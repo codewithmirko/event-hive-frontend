12875ed861cb4cd2c8fe85b6071969f67176eb4d
Mirko Schaefer <codewithmirko@gmail.com>
2024-04-30 13:10:12 +0200

    Add Search Box from Mantine

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,5 +1,14 @@
+import { Input } from "@mantine/core";
+
 const SearchBox = () => {
-  return <p>Searchbox is here</p>;
+  return (
+    <>
+      <p>Searchbox is here</p>
+      <Input component="button" pointer>
+        <Input.Placeholder>Placeholder content</Input.Placeholder>
+      </Input>
+    </>
+  );
 };
 
 export default SearchBox;
