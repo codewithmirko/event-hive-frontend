8fe744d399aa803e99d93d775d517789725a2396
johndaniell@yahoo.com <johndaniell@yahoo.com>
2024-04-30 10:49:11 +0200

    added mantine core

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createTheme, MantineProvider } from '@mantine/core';
 import App from './App.jsx'
-import './index.css'
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <MantineProvider theme={theme}>
     <App />
+    </MantineProvider>
   </React.StrictMode>,
 )
