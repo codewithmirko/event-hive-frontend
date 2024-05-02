b23b032b1a3bbe34317bb60213a9430a3a20400a
johndaniell@yahoo.com <johndaniell@yahoo.com>
2024-05-02 20:51:44 +0200

    -added new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import "@mantine/core/styles.css";
+import '@mantine/dates/styles.css';
+// import '@mantine/dropzone/styles.css';
+// import '@mantine/code-highlight/styles.css';
 import { Button } from "@mantine/core";
 import { Routes, Route } from "react-router-dom";
 
@@ -6,6 +9,8 @@ import HomePage from "./pages/HomePage";
 import ErrorPage from "./pages/ErrorPage";
 import Header from "./components/Header";
 import { EventProvider } from "./context/EventContext";
+import EventDetailPage from "./pages/EventDetailPage"
+import CreateEventPage from "./pages/CreateEventPage";
 
 function App() {
   return (
@@ -13,6 +18,8 @@ function App() {
       <Header />
       <EventProvider>
         <Routes>
+          <Route path="/event/:eventId" element={<EventDetailPage />} />
+          <Route path="/event/create" element={<CreateEventPage />} />
           <Route path="/" element={<HomePage />} />
           <Route path="*" element={<ErrorPage />} />
         </Routes>
