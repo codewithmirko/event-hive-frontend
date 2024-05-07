e3f9e61e17b125770acfc6a205448def28133d53
Mirko Schaefer <codewithmirko@gmail.com>
2024-05-07 10:38:15 +0200

    Change localhost to VITE_API_URL

--- src/context/auth.context.jsx
+++ src/context/auth.context.jsx
@@ -1,11 +1,9 @@
-import  { useState, useEffect, createContext  } from "react";
+import { useState, useEffect, createContext } from "react";
 import axios from "axios";
 import CustomNotification from "../components/CustomNotification";
 
-
-const API_URL = "http://localhost:5005";
+const API_URL = import.meta.env.VITE_API_URL;
 const AuthContext = createContext();
-
 
 function AuthProviderWrapper(props) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -19,52 +17,54 @@ function AuthProviderWrapper(props) {
   const authenticateUser = () => {
     // Get the stored token from the localStorage
     const storedToken = localStorage.getItem("authToken");
-  
+
     // Early exit if no token is found in localStorage
     if (!storedToken) {
       logOutUser("info", "No session found, please log in.");
-      return;  // Stop execution of the function here
+      return; // Stop execution of the function here
     }
-  
+
     // If the token exists, proceed to verify it with the server
-    axios.get(`${API_URL}/auth/verify`, {
-      headers: { Authorization: `Bearer ${storedToken}` },
-    })
-    .then(() => {
-      // Token is valid, proceed to fetch user details
-      fetchUserDetails(storedToken);
-      setIsLoggedIn(true);
-      setIsLoading(false);
-    })
-    .catch(error => {
-      // If the server sends an error response (invalid token)
-      logOutUser("error", "Session expired, please log in again.");
-      console.error('Error verifying token:', error);
-    });
+    axios
+      .get(`${API_URL}/auth/verify`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      .then(() => {
+        // Token is valid, proceed to fetch user details
+        fetchUserDetails(storedToken);
+        setIsLoggedIn(true);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        // If the server sends an error response (invalid token)
+        logOutUser("error", "Session expired, please log in again.");
+        console.error("Error verifying token:", error);
+      });
   };
-  
 
   const fetchUserDetails = (token) => {
-    axios.get(`${API_URL}/auth/user-details`, {
-      headers: { Authorization: `Bearer ${token}` }
-    })
-    .then(response => {
-      
-      const user = response.data.user;
-      // console.log('FETCHING USER ', user)
-      setUser(user);
-      setIsLoggedIn(true);
-      setIsLoading(false)
-    })
-    .catch(error => {
-      console.error('Error fetching user details:', error);
-      logOutUser("error", "Failed to fetch user details, logging out for security.");
-    })
-    .finally(() => {
-      setIsLoading(false);
-    });
+    axios
+      .get(`${API_URL}/auth/user-details`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        const user = response.data.user;
+        // console.log('FETCHING USER ', user)
+        setUser(user);
+        setIsLoggedIn(true);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching user details:", error);
+        logOutUser(
+          "error",
+          "Failed to fetch user details, logging out for security."
+        );
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
-
 
   const removeToken = () => {
     // <== ADD
@@ -72,18 +72,20 @@ function AuthProviderWrapper(props) {
     localStorage.removeItem("authToken");
   };
 
-const logOutUser = (notificationType = "success", notificationMessage = "You've been logged out.") => {
-  removeToken();
-  // console.log('loggingout')
-  setIsLoggedIn(false);
-  setUser(null);
-  setIsLoading(false);
-  CustomNotification({
-    type: notificationType,
-    message: notificationMessage
-  });
-};
-
+  const logOutUser = (
+    notificationType = "success",
+    notificationMessage = "You've been logged out."
+  ) => {
+    removeToken();
+    // console.log('loggingout')
+    setIsLoggedIn(false);
+    setUser(null);
+    setIsLoading(false);
+    CustomNotification({
+      type: notificationType,
+      message: notificationMessage,
+    });
+  };
 
   useEffect(() => {
     // console.log('HELLO')
--- src/pages/EventDetailPage.jsx
+++ src/pages/EventDetailPage.jsx
@@ -1,101 +1,111 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
-import { Container, Title, Text, Image, Badge, Card, Group, Button } from '@mantine/core';
-import { EventContext } from '../context/EventContext';
-import { AuthContext } from '../context/auth.context';  // Assuming you have a context to manage authentication
-import Comments from '../components/Comments';
+import React, { useState, useEffect, useContext } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import {
+  Container,
+  Title,
+  Text,
+  Image,
+  Badge,
+  Card,
+  Group,
+  Button,
+} from "@mantine/core";
+import { EventContext } from "../context/EventContext";
+import { AuthContext } from "../context/auth.context"; // Assuming you have a context to manage authentication
+import Comments from "../components/Comments";
 
 const EventDetailPage = () => {
-    const { eventId } = useParams();
-    const { updateEventParticipation } = useContext(EventContext);
-    const { user, isLoggedIn } = useContext(AuthContext); // Assuming this context provides user and authentication status
-    const [event, setEvent] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const [disableButton, setDisableButton] = useState(false);
+  const { eventId } = useParams();
+  const { updateEventParticipation } = useContext(EventContext);
+  const { user, isLoggedIn } = useContext(AuthContext); // Assuming this context provides user and authentication status
+  const [event, setEvent] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [disableButton, setDisableButton] = useState(false);
 
+  const fetchEvent = async () => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_API_URL}/api/events/${eventId}`
+      );
+      setEvent(response.data);
+      setLoading(false);
+    } catch (err) {
+      setError(err.message);
+      setLoading(false);
+    }
+  };
 
-    const fetchEvent = async () => {
-        try {
-            const response = await axios.get(`http://localhost:5005/api/events/${eventId}`);
-            setEvent(response.data);
-            setLoading(false);
-        } catch (err) {
-            setError(err.message);
-            setLoading(false);
-        }
-    };
+  useEffect(() => {
+    fetchEvent();
+  }, [eventId, updateEventParticipation]);
 
+  if (loading) return <Text>Loading...</Text>;
+  if (error) return <Text c="red">Error: {error}</Text>;
+  if (!event) return <Text>This event does not exist.</Text>;
 
-    useEffect(() => {
-        fetchEvent();
-    }, [eventId,updateEventParticipation]);
+  const isUserAttending = event.attendees.some((att) => att._id === user?._id); // Check if the user is in the attendees list
 
-    if (loading) return <Text>Loading...</Text>;
-    if (error) return <Text c="red">Error: {error}</Text>;
-    if (!event) return <Text>This event does not exist.</Text>;
+  const handleEventParticipation = async () => {
+    setDisableButton(true); // Disable the button immediately
 
-    const isUserAttending = event.attendees.some(att => att._id === user?._id); // Check if the user is in the attendees list
+    try {
+      const actionType = isUserAttending ? "leave" : "join";
+      await updateEventParticipation(event._id, actionType);
 
-    const handleEventParticipation = async () => {
-        setDisableButton(true); // Disable the button immediately
-    
-        try {
-            const actionType = isUserAttending ? 'leave' : 'join';
-            await updateEventParticipation(event._id, actionType);
-    
-            await fetchEvent(); // Optionally refetch event data to update the UI
-        } catch (error) {
-            console.error("Error during event participation:", error);
-            showNotification({
-                title: 'Error',
-                message: 'Failed to update event participation.',
-                color: 'red',
-            });
-        } finally {
-            setTimeout(() => setDisableButton(false), 2000); // Re-enable the button after 2 seconds
-        }
-    };
-    
+      await fetchEvent(); // Optionally refetch event data to update the UI
+    } catch (error) {
+      console.error("Error during event participation:", error);
+      showNotification({
+        title: "Error",
+        message: "Failed to update event participation.",
+        color: "red",
+      });
+    } finally {
+      setTimeout(() => setDisableButton(false), 2000); // Re-enable the button after 2 seconds
+    }
+  };
 
-    return (
-        <Container>
-            <Card shadow="sm" p="lg">
-                <Card.Section>
-                    <Image src={event.photo || '/default-event.jpg'} alt={event.eventname} height={200} />
-                </Card.Section>
-                <Group position="apart" style={{ marginBottom: 5, marginTop: 'md' }}>
-                    <Title order={2}>{event.eventname}</Title>
-                    <Badge color="pink" variant="light">
-                        {event.eventType}
-                    </Badge>
-                </Group>
-                <Text size="sm">{event.description}</Text>
-                <Text size="sm" style={{ color: 'gray', marginTop: 10 }}>
-                    Location: {event.location}
-                </Text>
-                <Text size="sm" style={{ color: 'gray' }}>
-                    Date: {event.date ? new Date(event.date).toLocaleDateString() : 'TBA'}
-                </Text>
-                <Text size="sm">
-                    Organizer: {event.organizer.username}
-                </Text>
-                <Text size="sm">
-                    Attendees: {event.attendees.map(a => a.username).join(", ")}
-                </Text>
-                <Button
-                    variant="outline"
-                    style={{ marginTop: 14 }}
-                    onClick={handleEventParticipation}
-                    disabled={!isLoggedIn || disableButton}  // Disable if not logged in
-                >
-                    {isUserAttending ? 'Leave Event' : 'Join Event'}  
-                </Button>
-            </Card>
-            <Comments eventId={event._id} />
-        </Container>
-    );
+  return (
+    <Container>
+      <Card shadow="sm" p="lg">
+        <Card.Section>
+          <Image
+            src={event.photo || "/default-event.jpg"}
+            alt={event.eventname}
+            height={200}
+          />
+        </Card.Section>
+        <Group position="apart" style={{ marginBottom: 5, marginTop: "md" }}>
+          <Title order={2}>{event.eventname}</Title>
+          <Badge color="pink" variant="light">
+            {event.eventType}
+          </Badge>
+        </Group>
+        <Text size="sm">{event.description}</Text>
+        <Text size="sm" style={{ color: "gray", marginTop: 10 }}>
+          Location: {event.location}
+        </Text>
+        <Text size="sm" style={{ color: "gray" }}>
+          Date: {event.date ? new Date(event.date).toLocaleDateString() : "TBA"}
+        </Text>
+        <Text size="sm">Organizer: {event.organizer.username}</Text>
+        <Text size="sm">
+          Attendees: {event.attendees.map((a) => a.username).join(", ")}
+        </Text>
+        <Button
+          variant="outline"
+          style={{ marginTop: 14 }}
+          onClick={handleEventParticipation}
+          disabled={!isLoggedIn || disableButton} // Disable if not logged in
+        >
+          {isUserAttending ? "Leave Event" : "Join Event"}
+        </Button>
+      </Card>
+      <Comments eventId={event._id} />
+    </Container>
+  );
 };
 
 export default EventDetailPage;
