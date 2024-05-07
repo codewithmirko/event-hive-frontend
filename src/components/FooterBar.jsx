c906d8f79b21ccf8816ba6a2e613ce32225e9655
Mirko Schaefer <codewithmirko@gmail.com>
2024-05-07 16:09:30 +0200

    Fix footer links, add mantine color overwrite, write readme, edit favicon

--- src/components/FooterBar.jsx
+++ src/components/FooterBar.jsx
@@ -1,32 +1,18 @@
-import { Container, Group, Anchor, Image } from "@mantine/core";
-import { MantineLogo } from "@mantinex/mantine-logo";
+import { Container, Group, Image } from "@mantine/core";
 import classes from "../styles/FooterBar.module.css";
 import logo from "../assets/logo-text-full-res.png";
-
-const links = [
-  { link: "#", label: "Contact" },
-  { link: "#", label: "About" },
-  { link: "#", label: "Privacy" },
-];
+import { Link } from "react-router-dom";
 
 function FooterBar() {
-  const items = links.map((link) => (
-    <Anchor
-      key={link.label}
-      href={link.link}
-      color="dimmed"
-      size="sm"
-      onClick={(event) => event.preventDefault()}
-    >
-      {link.label}
-    </Anchor>
-  ));
-
   return (
     <div className={classes.footer}>
       <Container className={classes.inner}>
-        <Image src={logo} h={35} w="auto" />
-        <Group className={classes.links}>{items}</Group>
+        <Link to="/">
+          <Image src={logo} h={35} w="auto" />
+        </Link>
+        <Group>
+          <Link to="/about">About</Link>
+        </Group>
       </Container>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -4,21 +4,34 @@ import { AuthProviderWrapper } from "./context/auth.context";
 import { EventProvider } from "./context/EventContext.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
 import { createTheme, MantineProvider } from "@mantine/core";
-import { Notifications  } from '@mantine/notifications';
+import { Notifications } from "@mantine/notifications";
 import App from "./App.jsx";
 
 const theme = createTheme({
-  /** Put your mantine theme override here */
+  colors: {
+    blue: [
+      "#FFF9DB",
+      "#FFF3BF",
+      "#FFEC99",
+      "#FFE066",
+      "#FFD43B",
+      "#FCC419",
+      "#FAB005",
+      "#F59F00",
+      "#F08C00",
+      "#E67700",
+    ],
+  },
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <MantineProvider theme={theme}>
-    <Notifications />
+      <Notifications />
       <Router>
         <AuthProviderWrapper>
           <EventProvider>
-          <App />
+            <App />
           </EventProvider>
         </AuthProviderWrapper>
       </Router>
