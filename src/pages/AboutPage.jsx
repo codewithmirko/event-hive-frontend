d9870704c1a74a8eee86ecba060ea1597c3d6b7e
Mirko Schaefer <codewithmirko@gmail.com>
2024-05-08 13:07:17 +0200

    Clean up about page code, remove uneccesary images

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,63 +3,17 @@ import {
   Group,
   Title,
   Text,
-  Card,
-  SimpleGrid,
   Container,
-  rem,
   useMantineTheme,
 } from "@mantine/core";
-import { IconGauge, IconUser, IconCookie } from "@tabler/icons-react";
 import classes from "../styles/AboutPage.module.css";
 import EmployeeCard from "../components/EmployeeCard";
 import mirkoAvatar from "../assets/mirko-picture.jpeg";
 import fatmaAvatar from "../assets/fatma-picture.jpeg";
 import danielAvatar from "../assets/daniel-picture.jpeg";
 
-const mockdata = [
-  {
-    title: "Extreme performance",
-    description:
-      "This dust is actually a powerful poison that will even make a pro wrestler sick, Regice cloaks itself with frigid air of -328 degrees Fahrenheit",
-    icon: IconGauge,
-  },
-  {
-    title: "Privacy focused",
-    description:
-      "People say it can run at the same speed as lightning striking, Its icy body is so cold, it will not melt even if it is immersed in magma",
-    icon: IconUser,
-  },
-  {
-    title: "No third parties",
-    description:
-      "They’re popular, but they’re rare. Trainers who show them off recklessly may be targeted by thieves",
-    icon: IconCookie,
-  },
-];
-
 function AboutPage() {
   const theme = useMantineTheme();
-  const features = mockdata.map((feature) => (
-    <Card
-      key={feature.title}
-      shadow="md"
-      radius="md"
-      className={classes.card}
-      padding="xl"
-    >
-      <feature.icon
-        style={{ width: rem(50), height: rem(50) }}
-        stroke={2}
-        color={theme.colors.blue[6]}
-      />
-      <Text fz="lg" fw={500} className={classes.cardTitle} mt="md">
-        {feature.title}
-      </Text>
-      <Text fz="sm" c="dimmed" mt="sm">
-        {feature.description}
-      </Text>
-    </Card>
-  ));
 
   return (
     <Container size="lg" py="xl">
