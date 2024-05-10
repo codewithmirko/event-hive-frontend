adbe3e75a0a28bdd6351f5bf5a4e88a6791619c6
johndaniell@yahoo.com <johndaniell@yahoo.com>
2024-05-10 10:58:51 +0200

    fixed user profile styling for small screens

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -5,20 +5,18 @@ const UserDetails = ({ user, onEdit }) => {
   return (
     <Paper shadow="sm" p="md" withBorder>
       <Grid align="center">
-        <Grid.Col span={3}>
+        <Grid.Col span={{ base: 12, sm: 3, md: 3, lg: 3 }}>
           <Avatar
-            src={`https://api.multiavatar.com/${encodeURIComponent(
-              user.username
-            )}.png`}
+            src={`https://api.multiavatar.com/${encodeURIComponent(user.username)}.png`}
             alt={`${user.username}'s avatar`}
             size={90}
             style={{ margin: "0 auto" }} // Center the avatar in its grid column
           />
-          <Text size="sm" align="center">
+          <Text size="sm" align="center" style={{ marginTop: 10 }}>
             {user.username}
           </Text>
         </Grid.Col>
-        <Grid.Col span={7}>
+        <Grid.Col span={{ base: 12, sm: 6, md: 6, lg: 6 }}>
           <Text size="sm">Location: {user.info.location}</Text>
           <Text size="sm">Age: {user.info.age}</Text>
           <Text size="sm">Hobbies: {user.info.hobbies.join(", ")}</Text>
@@ -27,12 +25,11 @@ const UserDetails = ({ user, onEdit }) => {
             Name: {user.info.firstName} {user.info.lastName}
           </Text>
         </Grid.Col>
-        <Grid.Col
-          span={2}
+        <Grid.Col span={{ base: 12, sm: 3, md: 3, lg: 3 }}
           style={{
             display: "flex",
             justifyContent: "flex-end",
-            alignItems: "flex-start",
+            alignItems: "center",
           }}
         >
           <Button
